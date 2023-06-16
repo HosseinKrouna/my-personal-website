648c9413f0f21a8548e5033b47dad2d5f7b9c746
HosseinKrouna <117902132+HosseinKrouna@users.noreply.github.com>
2023-06-16 09:31:06 +0200

    add: MainImage

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import Layout from "@/components/Layout";
 import Image from "next/image";
+import styled from "styled-components";
+
 export default function HomePage() {
 	return (
 		<Layout>
 			<h1>Welcome to My Personal Website!</h1>
-			<Image
-				src="/public/images/rafael-leao-PzmmiWoJHA8-unsplash.jpg"
+			<MainImage
+				src="/images/rafael-leao-PzmmiWoJHA8-unsplash.jpg"
 				alt="Picture in the sunset with a surfer"
-				width={100}
-				height={100}
+				width={1000}
+				height={200}
+				layout="responsive"
 			/>
 		</Layout>
 	);
 }
+
+const MainImage = styled(Image)``;

37f30efee1768fdffe5c21077df6997565570e32
HosseinKrouna <117902132+HosseinKrouna@users.noreply.github.com>
2023-06-23 10:25:47 +0200

    modify: profile description and address

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
 import Layout from "../components/Layout";
-import { Grid } from "@mui/material";
-import { ContentWrapper } from "@/components/styles/StyledAbout";
+import { Grid, Box } from "@mui/material";
+import {
+	ContentWrapper,
+	HighlightedText,
+} from "@/components/styles/StyledAbout";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 const cardVariants = {
@@ -9,9 +12,9 @@ const cardVariants = {
 };
 
 function AboutPage() {
-	const { scrollY } = useScroll();
-	const opacity = useTransform(scrollY, [100, 200], [0, 1]);
-
+	const { scrollYProgress } = useScroll();
+	const opacity = useTransform(scrollYProgress, [0.5, 0.7], [0, 1]);
+	const translateY = useTransform(scrollYProgress, [0.5, 0.7], [50, 0]);
 	return (
 		<Layout>
 			<ContentWrapper>
@@ -21,78 +24,138 @@ function AboutPage() {
 							variants={cardVariants}
 							initial="hidden"
 							animate="visible"
-							style={{ opacity }}
+							style={{ opacity, y: translateY }}
 						>
-							<h2>About Me</h2>
+							<h2>
+								About <HighlightedText>Me</HighlightedText>
+							</h2>
+
+							<p>Hello there!</p>
 							<p>
-								{`Hello there! My name is Hossein Krouna. I have a passion for frontend
-				development and strive for continuous growth in this field. When I'm not
-				coding, I transform into a superhero dad, rescuing my two little ones
-				from the daily adventures of life. Interestingly, my kids are also
-				starting to show an interest in programming. Recognizing the importance
-				of the IT industry for the future, I am excited to share my knowledge
-				and inspire them on this journey. I believe in the power of learning and
-				am committed to expanding my skills every day. With dedication and
-				enthusiasm, I aim to make a positive impact through my contributions in
-				the world of coding.`}
+								My name is Hossein Krouna.
+								<br />I have a passion for frontend development and strive for
+								continuous growth in this field. When
+								{` I'm`} not coding, I transform into a superhero dad, rescuing
+								my two little ones from the daily adventures of life.
+							</p>
+							<p>
+								Interestingly, my kids are also starting to show an interest in
+								programming. Recognizing the importance of the IT industry for
+								the future, I am excited to share my knowledge and inspire them
+								on this journey.
+							</p>
+							<p>
+								I believe in the power of learning and am committed to expanding
+								my skills every day. With dedication and enthusiasm, I aim to
+								make a positive impact through my contributions in the world of
+								coding.
 							</p>
 						</motion.div>
 					</Grid>
-					<Grid item xs={6} sm={3}>
-						<h3>Age</h3>
-						<p>40</p>
-					</Grid>
-					<Grid item xs={6} sm={3}>
-						<h3>Residence</h3>
-						<p>Germany</p>
-					</Grid>
-					<Grid item xs={6} sm={3}>
-						<h3>Address</h3>
-						<p>88 Some Street, Some Town</p>
-					</Grid>
-					<Grid item xs={6} sm={3}>
-						<h3>Email</h3>
-						<p>email@example.com</p>
-					</Grid>
-					<Grid item xs={6} sm={3}>
-						<h3>Phone</h3>
-						<p>+0123 123 456 789</p>
-					</Grid>
-					<Grid item xs={12}>
-						<h2>What I Do</h2>
-					</Grid>
-					<Grid item xs={12} sm={6}>
-						<h3>Enjoy the Life</h3>
-						<p>
-							Pellentesque pellentesque, ipsum sit amet auctor accumsan, odio
-							tortor bibendum massa, sit amet ultricies ex lectus scelerisque
-							nibh. Ut non sodales.
-						</p>
-					</Grid>
-					<Grid item xs={12} sm={6}>
-						<h3>Web Design</h3>
-						<p>
-							Pellentesque pellentesque, ipsum sit amet auctor accumsan, odio
-							tortor bibendum massa, sit amet ultricies ex lectus scelerisque
-							nibh. Ut non sodales.
-						</p>
-					</Grid>
-					<Grid item xs={6} sm={3}>
-						<h3>Copywriting</h3>
-						<p>
-							Pellentesque pellentesque, ipsum sit amet auctor accumsan, odio
-							tortor bibendum massa, sit amet ultricies ex lectus scelerisque
-							nibh. Ut non sodales.
-						</p>
-					</Grid>
-					<Grid item xs={6} sm={3}>
-						<h3>Management</h3>
-						<p>
-							Pellentesque pellentesque, ipsum sit amet auctor accumsan, odio
-							tortor bibendum massa, sit amet ultricies ex lectus scelerisque
-							nibh. Ut non sodales.
-						</p>
-					</Grid>
+					<Box sx={{ flexGrow: 1 }}>
+						<Grid container mt={8} ml={8}>
+							<motion.div
+								variants={cardVariants}
+								initial="hidden"
+								animate="visible"
+								style={{ opacity, y: translateY }}
+							>
+								<Grid item container xs={12}>
+									<Grid item xs={3}>
+										<h3>Age</h3>
+									</Grid>
+									<Grid item xs={9}>
+										<p>40</p>
+									</Grid>
+								</Grid>
+
+								<Grid item container xs={12} sx={{ my: -4 }}>
+									<Grid item xs={3}>
+										<h3>Residence</h3>
+									</Grid>
+									<Grid item xs={9}>
+										<p>Germany</p>
+									</Grid>
+								</Grid>
+
+								<Grid item container xs={12} sx={{ my: -4 }}>
+									<Grid item xs={3}>
+										<h3>Address</h3>
+									</Grid>
+									<Grid item xs={9}>
+										<p>88 Some Street, Some Town</p>
+									</Grid>
+								</Grid>
+
+								<Grid item container xs={12} sx={{ my: -4 }}>
+									<Grid item xs={3}>
+										<h3>Email</h3>
+									</Grid>
+									<Grid item xs={9}>
+										<p>email@example.com</p>
+									</Grid>
+								</Grid>
+
+								<Grid item container xs={12} sx={{ my: -4 }}>
+									<Grid item xs={3}>
+										<h3>Phone</h3>
+									</Grid>
+									<Grid item xs={9}>
+										<p>+0123 123 456 789</p>
+									</Grid>
+								</Grid>
+							</motion.div>
+						</Grid>
+					</Box>
+
+					<Box sx={{ flexGrow: 1 }}>
+						<Grid container mt={8} ml={8}>
+							<motion.div
+								variants={cardVariants}
+								initial="hidden"
+								animate="visible"
+								style={{ opacity, y: translateY }}
+							>
+								<Grid item xs={12}>
+									<h2>
+										What <HighlightedText>I Do</HighlightedText>
+									</h2>
+								</Grid>
+								<Grid item xs={12} sm={6}>
+									<h3>Enjoy the Life</h3>
+									<p>
+										Pellentesque pellentesque, ipsum sit amet auctor accumsan,
+										odio tortor bibendum massa, sit amet ultricies ex lectus
+										scelerisque nibh. Ut non sodales.
+									</p>
+								</Grid>
+								<Grid item xs={12} sm={6}>
+									<h3>Web Design</h3>
+									<p>
+										Pellentesque pellentesque, ipsum sit amet auctor accumsan,
+										odio tortor bibendum massa, sit amet ultricies ex lectus
+										scelerisque nibh. Ut non sodales.
+									</p>
+								</Grid>
+								<Grid item xs={6} sm={3}>
+									<h3>Copywriting</h3>
+									<p>
+										Pellentesque pellentesque, ipsum sit amet auctor accumsan,
+										odio tortor bibendum massa, sit amet ultricies ex lectus
+										scelerisque nibh. Ut non sodales.
+									</p>
+								</Grid>
+								<Grid item xs={6} sm={3}>
+									<h3>Management</h3>
+									<p>
+										Pellentesque pellentesque, ipsum sit amet auctor accumsan,
+										odio tortor bibendum massa, sit amet ultricies ex lectus
+										scelerisque nibh. Ut non sodales.
+									</p>
+								</Grid>
+							</motion.div>
+						</Grid>
+					</Box>
 				</Grid>
 			</ContentWrapper>
 		</Layout>
